7490f9e2a5409aa80253e622d2745235e66bf515
Protus bantan <protus.bantan@gmail.com.com>
2018-09-14 17:58:00 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask_script import Manager,Server
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+    #....
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+bootstrap = Bootstrap()
+
+# Creating app instance
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+# Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    return app
---
+++ app/main/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from . import main
+from flask_login import login_required
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
+
+
