2e41360f5a19204e92e4da036a5f1622db18e043
Protus bantan <protus.bantan@gmail.com.com>
2018-09-17 19:30:16 +0300

    init

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 class DevConfig(Config):
