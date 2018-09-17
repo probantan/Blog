1cd09aaa731adf32b38068d36bb1dc446d6cdc89
Protus bantan <protus.bantan@gmail.com.com>
2018-09-17 18:55:51 +0300

    pushunging to heroku

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/blog'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
@@ -13,7 +13,7 @@ class DevConfig(Config):
     DEBUG = True
 
 config_options = {
-'development':DevConfig,    
+#'development':DevConfig,    
 'production':ProdConfig,
 }
 
